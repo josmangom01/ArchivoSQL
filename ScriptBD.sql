a9d8459c68060c5660989e6155bf93e7ea6c93fb
josmangom01 <83991742+josmangom01@users.noreply.github.com>
2023-09-23 14:06:58 -0600

    Ampliar Catalogo

--- ScriptBD.sql
+++ ScriptBD.sql
@@ -16,7 +16,9 @@ create table pedidos(
 );
 
 insert into flautas values
-    (null, "A", 600.00,  "https://e7.pngegg.com/pngimages/737/573/png-clipart-brown-flute-flute-wood-objects-music.png"),
-    (null, "B", 800.00, "https://w7.pngwing.com/pngs/239/800/png-transparent-flute-quena-yellow-flute-angle-yellow-flowers-wood.png"),
-    (null, "C", 1000, "https://i0.wp.com/elhogardelaflautatravesera.com/wp-content/uploads/2022/02/IMG_0493.jpeg?fit=1000%2C750&ssl=1");
+    (null, "A", 600.00,  "Imagenes/Modelo-A.jpg"),
+    (null, "B", 800.00, "Imagenes/Modelo-B.jpg"),
+    (null, "C", 1000, "Imagenes/Modelo-C.jpg"),
+    (null, "D", 1500, "Imagenes/Modelo-D.jpg"),
+    (null, "E", 2000, "Imagenes/Modelo-E.jpg");
 
