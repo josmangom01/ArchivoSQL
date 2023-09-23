c62ebca0066fa98cf9cf8c6aed6b73b6ac8f6cce
josmangom01 <83991742+josmangom01@users.noreply.github.com>
2023-09-22 23:55:06 -0600

    inicial

---
+++ ScriptBD.sql
@@ -0,0 +1,22 @@
+create table flautas (
+    id bigint unsigned not null auto_increment primary key,
+    modelo varchar(20) unique,
+    precio double,
+    imagen varchar(200)
+);
+
+
+create table pedidos(
+    id bigint unsigned not null auto_increment primary key,
+    nombre varchar(50),
+    modelo varchar(20),
+    cantidad int,
+    FOREIGN KEY (modelo) REFERENCES flautas (modelo)
+    
+);
+
+insert into flautas values
+    (null, "A", 600.00, "https://i0.wp.com/elhogardelaflautatravesera.com/wp-content/uploads/2022/02/IMG_0493.jpeg?fit=1000%2C750&ssl=1"),
+    (null, "B", 800.00, "https://w7.pngwing.com/pngs/239/800/png-transparent-flute-quena-yellow-flute-angle-yellow-flowers-wood.png"),
+    (null, "C", 1000.00,  "https://e7.pngegg.com/pngimages/737/573/png-clipart-brown-flute-flute-wood-objects-music.png");
+
